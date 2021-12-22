cadb98570529779f6c0696536bd289a3663cdde2
Sue-52 <1219243947@qq.com>
2021-12-22 09:56:18 +0800

    feat: normalize、md
    添加默认样式清除、md 文件解析

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import "./plugins/axios";
 import { createApp } from "vue";
+// 清除样式引入
 import "normalize.css";
+// MD文件样式引入
+import 'github-markdown-css';
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
