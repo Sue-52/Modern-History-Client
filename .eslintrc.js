82ccfdae5370412f006db5ff17ad6b680d3fea81
Sue-52 <1219243947@qq.com>
2021-12-22 09:01:58 +0800

    213

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "@typescript-eslint/ban-ts-comment": 0, //允许忽略错误
+    "@typescript-eslint/no-explicit-any": 0, // 允许 any 类型
+    "@typescript-eslint/explicit-module-boundary-types": 0, //形参允许 any
+  },
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">123123</div>
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+
+
+
+
+
+
+  
+  <div>5</div>
+</template>
+
+<script lang="ts"></script>
+
+<style lang="scss">
+div {
+  color: $redColor;
+}
+</style>
